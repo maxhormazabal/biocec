aa796111f22dd6ad8e0173bdb3f92b77eecafe5c
maxhormazabal <mhormazabal.lagos@gmail.com>
2025-04-09 16:07:05 +0200

    renombrado de documentos

--- js/documentos.js
+++ js/documentos.js
@@ -2,55 +2,70 @@
 const pdfDocuments = [
   {
     name: "Reglamento Interno",
-    path: "./documentos/BIOCEC Reglamento interno v1.0 29.oct.2024.pdf",
+    path: "./documentos/BIOCEC_Reglamento_interno_v10_29oct2024.pdf",
+    downloaded_filename: "BIOCEC_Reglamento_interno_v10_29oct2024",
   },
   {
     name: "Carta de Compromiso Investigador Principal y Sub Investigador",
-    path: "./documentos/Carta de Compromiso Investigador Principal y Sub Investigador.pdf",
+    path: "./documentos/Carta_de_Compromiso_Investigador_Principal_y_Sub_Investigador.pdf",
+    downloaded_filename:
+      "Carta_de_Compromiso_Investigador_Principal_y_Sub_Investigador",
   },
   {
     name: "Carta Sometimiento Estudio Inicial",
-    path: "./documentos/Carta Sometimiento Estudio Inicial.pdf",
+    path: "./documentos/Carta_Sometimiento_Estudio_Inicial.pdf",
+    downloaded_filename: "Carta_Sometimiento_Estudio_Inicial",
   },
   {
     name: "Declaración No Conflicto de Interés Investigador Principal y Sub-Investigador",
-    path: "./documentos/Declaración No Conflicto de Interés Investigador Principal y Sub-Investigador.pdf",
+    path: "./documentos/Declaracion_No_Conflicto_de_Interes_Investigador_Principal_y_SubInvestigador.pdf",
+    downloaded_filename:
+      "Declaracion_No_Conflicto_de_Interes_Investigador_Principal_y_SubInvestigador",
   },
   {
     name: "Notificación Cierre de Centro",
-    path: "./documentos/Notificación Cierre de Centro.pdf",
+    path: "./documentos/Notificacion_Cierre_de_Centro.pdf",
+    downloaded_filename: "Notificacion_Cierre_de_Centro",
   },
   {
     name: "Notificación Cierre de Estudio",
-    path: "./documentos/Notificación Cierre de Estudio.pdf",
+    path: "./documentos/Notificacion_Cierre_de_Estudio.pdf",
+    downloaded_filename: "Notificacion_Cierre_de_Estudio",
   },
   {
     name: "Notificación de Renovación Anual",
-    path: "./documentos/Notificación de Renovación Anual.pdf",
+    path: "./documentos/Notificacion_de_Renovacion_Anual.pdf",
+    downloaded_filename: "Notificacion_de_Renovacion_Anual",
   },
   {
     name: "Notificación Eventos Adversos Serios",
-    path: "./documentos/Notificación Eventos Adversos Serios.pdf",
+    path: "./documentos/Notificacion_Eventos_Adversos_Serios.pdf",
+    downloaded_filename: "Notificacion_Eventos_Adversos_Serios",
   },
   {
     name: "Calendario Sesiones 2025",
-    path: "./documentos/Calendario Sesiones BIOCEC 2025.pdf",
+    path: "./documentos/Calendario_Sesiones_BIOCEC_2025.pdf",
+    downloaded_filename: "Calendario_Sesiones_BIOCEC_2025",
   },
   {
     name: "Cumplimiento GCP 2025",
-    path: "./documentos/Cumplimiento GCP BIOCEC 2025.pdf",
+    path: "./documentos/Cumplimiento_GCP_BIOCEC_2025.pdf",
+    downloaded_filename: "Cumplimiento_GCP_BIOCEC_2025",
   },
   {
     name: "Nomina Integrantes 2025",
-    path: "./documentos/Nomina Integrantes BIOCEC 2025.pdf",
+    path: "./documentos/Nomina_Integrantes_BIOCEC_2025.pdf",
+    downloaded_filename: "Nomina_Integrantes_BIOCEC_2025",
   },
   {
     name: "Resolución 250835574",
     path: "./documentos/RESOLUCION_250835574.pdf",
+    downloaded_filename: "RESOLUCION_250835574",
   },
   {
     name: "Valores 2025",
-    path: "./documentos/Valores BIOCEC 2025.pdf",
+    path: "./documentos/Valores_BIOCEC_2025.pdf",
+    downloaded_filename: "Valores_BIOCEC_2025",
   },
 ];
 
@@ -79,7 +94,7 @@ function generatePDFList(documents) {
     downloadLink.href = doc.path;
     downloadLink.className = "bg-primary rounded-pill text-light px-2";
     downloadLink.textContent = "Descargar";
-    downloadLink.download = doc.name;
+    downloadLink.download = doc.downloaded_filename;
 
     // Agregar los elementos al elemento <li>
     listItem.appendChild(docInfo);
